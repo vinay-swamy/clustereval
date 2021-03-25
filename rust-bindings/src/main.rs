d21f84ffe4a77fde0f63a4f6abb79fa770a7ebb5
Swamy <swamyvinny@gmail.com>
2021-03-24 23:45:47 -0400

    making progress

--- rust-bindings/src/main.rs
+++ rust-bindings/src/main.rs
@@ -3,20 +3,45 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::iter::FromIterator;
 use std::iter::Iterator;
+use ndarray::{array,Array2, s};
 
 #[derive(Debug)]
 
 struct ClusterResults {
     barcodes:Vec<String>,
     labels: Vec<String> , 
-    barcode_set:HashSet<String>
+    barcode_set:HashSet<String>,
+    grouped_barcodes: HashMap<String, HashSet<String>>
 }
+
+// struct ExperimentResults{
+//     exp_param :String,
+//     h_k_scores: &[f64]
+// }
 
 
 impl ClusterResults{
     fn new(barcodes:Vec<String>, labels: Vec<String>) -> ClusterResults{
         let barcode_set: HashSet<String> = HashSet::from_iter(barcodes.clone());
-        ClusterResults{barcodes, labels, barcode_set }
+        let mut grouped_barcodes:HashMap<String, HashSet<String>> = HashMap::new();
+        let mut old_label = &labels[0];
+        let mut current_label = &labels[0];// declare out here so we can add the last set back in 
+        let mut current_set: HashSet<String> = HashSet::new();
+        for i in 0..barcodes.len(){
+            current_label = &labels[i];
+            let current_barcode = &barcodes[i];
+            if  current_label == old_label{
+                current_set.insert(current_barcode.clone());
+            }else{// reach a new cluster 
+                grouped_barcodes.insert(old_label.clone(), current_set);
+                let ns: HashSet<String> = HashSet::new();
+                current_set = ns;
+                current_set.insert(current_barcode.clone());
+                old_label = current_label;
+            }
+        }
+        grouped_barcodes.insert(current_label.clone(), current_set);
+        ClusterResults{barcodes, labels, barcode_set, grouped_barcodes}
     }
     fn head(&self){
         println!("{:?}", &self.barcodes[0..5]);
@@ -40,7 +65,7 @@ impl ClusterResults{
         return res * -1 as f64
     }
     
-    fn H_tot(&self, query:ClusterResults) -> f64{
+    fn H_k(&self, query:&ClusterResults) -> f64{
         let intersect: HashSet<String> = self.barcode_set.intersection(&query.barcode_set).cloned().collect::<HashSet<String>>();
         println!("Helo{}" , intersect.len());
         if intersect.len() == 0{
@@ -66,21 +91,31 @@ impl ClusterResults{
 fn read_cluster_results( file: &str) ->ClusterResults {
     let file_string = fs::read_to_string(file).expect("Bad input file ");
     let file_string: Vec<&str> = file_string.lines().collect();
-    let mut barcodes = Vec::new();
-    let mut labels = Vec::new();
-    for i in 1..file_string.len(){
+    let mut barcodes: Vec<String> = vec![String::new(); file_string.len()];
+    let mut labels: Vec<String> = vec![String::new(); file_string.len()];
+    for i in 0..file_string.len(){
         let line_split : Vec<&str> = file_string[i].split(",").collect();
-        barcodes.push(String::from(line_split[0]));
-        labels.push(String::from(format!("clu{}", line_split[1]) ));
+        barcodes[i] = String::from(line_split[0]);
+        labels[i] = String::from(format!("clu{}", line_split[1]) );
     }
     ClusterResults::new(barcodes,labels)
 }
-//read a data frame into a ha 
 
+// fn run_calculation(ref_cluster:&ClusterResults, query_clusters: &Vec<ClusterResults>, h_tot_scores: &[f64], exp_name: String) -> ExperimentResults{
+
+// }
 
 fn main() {
-    let k = read_cluster_results("test.csv");
-    let j = read_cluster_results("query.csv");
-    println!("{}", k.H_tot(j))
-
+    let k = read_cluster_results("test_sorted.csv");
+    //let j = read_cluster_results("query.csv");
+    let j:usize = k.grouped_barcodes.values().map(|x| x.len() ).sum();
+    let mut x : Vec<usize> = k.grouped_barcodes.values().map(|x| x.len() ).collect();
+    x.sort();
+    let m = k.barcodes.len();    
+    //let p = k.grouped_barcodes.get(&String::from("clu34"));
+    println!("{:?}", j);
+    println!("{:?}", m)
+    //println!(xv)
+    //println!("{:?}", k.)
+    //println!("{}", m)
 }
